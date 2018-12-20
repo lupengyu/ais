202559ac599e71d25873bb2b08e1030718d7635e
卢鹏宇 <lupengyu.office@bytedance.com>
2018-12-20 18:45:56 +0800

    2018-12-20 18-34
    Change-Id: Ibb04a782cab7d840f4e550ade60e619fb854fc8f

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+)
 
 func main() {
-	fmt.Println("hello world")
+	coon, err := net.Dial("tcp", "123.206.67.38:7782")
+	fmt.Println(err)
+	defer coon.Close()
+	fmt.Println(coon)
+	//respBuff := make([]byte, 1024)
+	//n, err := coon.Read(respBuff)
+	//fmt.Println(err)
+	//fmt.Print(string(respBuff[:n]))
+	//for {
+	//	for n != 0 {
+	//		fmt.Print(string(respBuff[:n]))
+	//		n, _ = coon.Read(respBuff)
+	//	}
+	//
+	//}
 }
