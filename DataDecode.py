8e4fae54c4a7b38773859da526e9ca00b0719a57
卢鹏宇 <lupengyu.office@bytedance.com>
2018-12-27 00:13:01 +0800

    feat: add ais 4, 5, 11 decoder
    Change-Id: Ide8b165418e698231bf329b6d29b813878616adf

--- DataDecode.py
+++ DataDecode.py
@@ -1,18 +1,30 @@
 import Decoder
+import time
 
-f = open("data/11.25 20.56.txt")
+starttime = time.time()
 fwirt = open("main.csv", "w+")
 flog = open("decodelog.txt", "w+")
-line = f.readline()
-sum = 0
-while line:
-    Decoder.AIS_Decoder(line, fwirt, flog)
+finfo = open("info.csv", "w+")
+filecnt = 0
+for i in range(10000):
+    try:
+        f = open("data/f" + str(i) + ".txt")
+    except FileNotFoundError:
+        continue
+    print(str(i) + ".txt")
+    filecnt += 1
     line = f.readline()
-    sum += 1
-    if sum % 100 == 0:
-        print(sum)
-
-f.close()
+    sum = 0
+    while line:
+        Decoder.AIS_Decoder(line, fwirt, finfo, flog)
+        line = f.readline()
+        sum += 1
+        if sum % 10000 == 0:
+            print(sum)
+    f.close()
 fwirt.close()
 flog.close()
+endtime = time.time()
+print("File cnt:", filecnt)
+print("Spend time:", endtime-starttime)
 print(sum, Decoder.getTypeDict())
--- Decoder.py
+++ Decoder.py
@@ -66,10 +66,89 @@ ASCII_TO_6BITS = {
     'w': "111111"
 }
 
+BITS_TO_ASCII = {
+    '000000': "@",
+    '000001': "A",
+    '000010': "B",
+    '000011': "C",
+    '000100': "D",
+    '000101': "E",
+    '000110': "F",
+    '000111': "G",
+    '001000': "H",
+    '001001': "I",
+    '001010': "J",
+    '001011': "K",
+    '001100': "L",
+    '001101': "M",
+    '001110': "N",
+    '001111': "O",
+    '010000': "P",
+    '010001': "Q",
+    '010010': "R",
+    '010011': "S",
+    '010100': "T",
+    '010101': "U",
+    '010110': "V",
+    '010111': "W",
+    '011000': "X",
+    '011001': "Y",
+    '011010': "Z",
+    '011011': "[",
+    '011100': "\\",
+    '011101': "]",
+    '011110': "^",
+    '011111': "_",
+    '100000': " ",
+    '100001': "!",
+    '100010': "\"",
+    '100011': "#",
+    '100100': "$",
+    '100101': "%",
+    '100110': "&",
+    '100111': "`",
+    "101000": "(",
+    '101001': ")",
+    '101010': "*",
+    '101011': "+",
+    '101100': ",",
+    '101101': "-",
+    '101110': ".",
+    '101111': "/",
+    '110000': "0",
+    '110001': "1",
+    '110010': "2",
+    '110011': "3",
+    '110100': "4",
+    '110101': "5",
+    '110110': "6",
+    '110111': "7",
+    '111000': "8",
+    '111001': "9",
+    '111010': ":",
+    '111011': ";",
+    '111100': "<",
+    '111101': "=",
+    '111110': ">",
+    '111111': "?"
+}
+
+
 typeDict = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0,
                 9: 0, 10: 0, 11: 0, 12: 0, 13: 0, 14: 0, 15: 0,
                 16: 0, 17: 0, 18: 0, 19: 0, 20: 0, 21: 0, 22: 0,
                 23: 0, 24: 0, 25: 0, 26: 0, 27: 0, 28: 0, 29: 0, 30: 0}
+id = 1
+infoid = 1
+year = 0
+month = 0
+day = 0
+hour = 0
+minute = 0
+sec = 0
+tmpmessage = ""
+tmpbit = []
+waitSingle = 0
 
 def getTypeDict():
     return typeDict
@@ -124,68 +203,74 @@ def CRC_check(CRC, item):
     # print(CRC)
     return CRC
 
-def AIS_Decoder(message, f, log):
-    # print("Decoded message:")
-    # print(message)
+def AIS_Decoder(message, f, finfo, log):
+    global id
+    global year
+    global month
+    global day
+    global hour
+    global minute
+    global sec
+    global tmpbit
+    global waitSingle
+    global tmpmessage
+    global infoid
     messages = message.split(",")
     # 判断是否具有主要内容
     if len(messages) < 6:
-        # print("Unable to decode message")
         return
 
     # 判断标识符定义是否正确
     if messages[0] != "!AIVDM" and messages[0] != "!AIVDO":
-        # print("Invalid data packet ID")
         return
 
     # 判断语句数是否正确
     sum = 0
     if messages[1].isdigit():
         sum = int(messages[1])
+        #TODO: 目前只实现了单AIS编码
+        # if sum != 1:
+        #     return
         if sum <= 0 or sum > 9:
-            # print("Need more data for parsing")
             return
     else:
-        # print("Invalid number of fragments")
         return
 
     # 判断序列牌号是否正确
     cnt = 0
     if messages[2].isdigit():
         cnt = int(messages[2])
+        # if cnt != 1:
+        #     return
         if cnt <= 0 or cnt > sum:
-            # print("Need more data for parsing")
             return
     else:
-        # print("Invalid number of fragments")
         return
     
     # 判断统一标识是否合法
     if messages[3].isdigit():
         if int(messages[3]) < 0 or int(messages[3]) > 9:
-            # print("Need more data for parsing")
             return
     else:
         if messages[3] != "":
-            # print("Invalid number of fragments")
             return
 
     # 判断接收频道是否正确
     if messages[4] != "A" and messages[4] != "B" and messages[4] != "":
-        # print("Invalid radio channel code")
         return
     
     # 封装数据处理
     bits = ""
     if len(messages) < 7:
         return
-    # print()
+
     check = messages[6].split("*")
     CRC = "111111111111"
     for item in messages[5]:
-        if item == '!':
+        try:
+            bits += ASCII_TO_6BITS[item]
+        except KeyError:
             return
-        bits += ASCII_TO_6BITS[item]
         # if len(check) > 1:
         #     CRC = CRC_check(CRC, ASCII_TO_6BITS[item])
 
@@ -202,6 +287,70 @@ def AIS_Decoder(message, f, log):
         # if(len(check) > 1):
         #     print("CRC:", CRC)
         #     print("crc: %s%s"%(ASCII_TO_6BITS[check[1][0]], ASCII_TO_6BITS[check[1][1]]))
+    if waitSingle != 0:
+        if waitSingle == 5:
+            #348 84
+            if sum != 2 or cnt != 2:
+                return
+            zhuanfa = tmpbit[6:8] #2
+            MMSI = tmpbit[8:38] #30
+            AIS = tmpbit[38:40] #2
+            IMO = tmpbit[40:70] #30
+            huhao = tmpbit[70:112] #42
+            huhaostring = ""
+            isum = 0
+            for i in range(7):
+                try:
+                    huhaostring += BITS_TO_ASCII[huhao[isum:isum + 6]]
+                    isum += 6
+                except KeyError:
+                    return
+            name = tmpbit[112:232] #120
+            namestring = ""
+            isum = 0
+            for i in range(20):
+                try:
+                    namestring += BITS_TO_ASCII[name[isum:isum + 6]]
+                    isum += 6
+                except KeyError:
+                    return
+            type = tmpbit[232:240] #8
+            size = tmpbit[240:270]
+            A = size[0:9]
+            B = size[9:18]
+            C = size[18:24]
+            D = size[24:30]
+            position = tmpbit[270:274]
+            ETA = tmpbit[274:294]
+            ETA_min = ETA[14:20]
+            ETA_hour = ETA[9:14]
+            ETA_day = ETA[4:9]
+            ETA_month = ETA[0:4]
+            chishui = tmpbit[294:302]
+            des1 = tmpbit[302:348]
+            des = (des1 + bits)[:120]
+            desstring = ""
+            isum = 0
+            for i in range(20):
+                try:
+                    desstring += BITS_TO_ASCII[des[isum:isum + 6]]
+                    isum += 6
+                except KeyError:
+                    return
+            writeString = str(infoid) + "," + str(bits_to_numbers(zhuanfa)) + "," + str(bits_to_numbers(MMSI)) + "," + \
+                        str(bits_to_numbers(AIS)) + "," + str(bits_to_numbers(IMO)) + "," + huhaostring + "," + \
+                        namestring + "," + str(bits_to_numbers(type)) + "," + str(bits_to_numbers(A)) + "," + \
+                        str(bits_to_numbers(B)) + "," + str(bits_to_numbers(C)) + "," + str(bits_to_numbers(D)) + "," + \
+                        str(bits_to_numbers(A) + bits_to_numbers(B)) + "," + str(bits_to_numbers(C) + bits_to_numbers(D)) + "," + \
+                        str(bits_to_numbers(position)) + "," + str(bits_to_numbers(ETA_month)) + "," + str(bits_to_numbers(ETA_day)) + "," + str(bits_to_numbers(ETA_hour)) + "," + \
+                        str(bits_to_numbers(ETA_min)) + "," + str(bits_to_numbers(chishui) * 0.1) + "," + str(year) + "," + str(month) + "," + str(day) + "," + str(hour) + "," + str(minute) + "," + str(sec) + \
+                        "\r\n"
+            finfo.write(writeString)
+            log.write(tmpmessage)
+            log.write(message + "decode: " + writeString)
+            infoid += 1
+        waitSingle = 0
+        return
     Message_Type = bits[0:6]
     if bits_to_numbers(Message_Type) > 30 or bits_to_numbers(Message_Type) <= 0:
         return
@@ -209,95 +358,79 @@ def AIS_Decoder(message, f, log):
     if(bits_to_numbers(Message_Type) == 1 or
         bits_to_numbers(Message_Type) == 2 or
             bits_to_numbers(Message_Type) == 3):
+        if sum != 1 or cnt != 1:
+            return
         if len(bits) < 168:
             return
-        Repeat_Indicator = bits[6:8]
-        MMSI = bits[8:38]
-        Navigation_Status = bits[38:42]
-        ROT = bits[42:50]
+        Repeat_Indicator = bits[6:8] #2
+        MMSI = bits[8:38] #30
+        Navigation_Status = bits[38:42] #4
+        ROT = bits[42:50] #8
         # ROT = "01111111"
-        SOG = bits[50:60]
-        Position_Accuracy = bits[60]
-        Longitude = bits[61:89]
-        Latitude = bits[89:116]
-        COG = bits[116:128]
-        HDG = bits[128:137]
-        Time_Stamp = bits[137:143]
-        Reserved_for_regional = bits[143:147]
-        #147位未使用
+        SOG = bits[50:60] #10
+        Position_Accuracy = bits[60] #1
+        Longitude = bits[61:89] #28
+        Latitude = bits[89:116] #27
+        COG = bits[116:128] #12
+        HDG = bits[128:137] #9
+        Time_Stamp = bits[137:143] #6
+        Reserved_for_regional = bits[143:145] #3
+        #145-147位未使用
         RAIM_flag = bits[148]
         Communication_State = bits[149:168]
         Sync_state = Communication_State[0:2]
         Slot_Timeout = Communication_State[2:5]
         UTC_hour = Communication_State[5:10]
-        UTC_minute = Communication_State[10:17]
+        UTC_minute = Communication_State[10:19]
 
-        writeString = str(bits_to_numbers(Message_Type)) + "," + \
+        writeString = str(id) + "," + str(bits_to_numbers(Message_Type)) + "," + \
                       str(bits_to_numbers(Repeat_Indicator)) + "," + \
                       str(bits_to_numbers(MMSI)) + "," + \
                       str(bits_to_numbers(Navigation_Status)) + "," + \
                       str(bits_to_Complementnumbers(ROT)) + "," + \
-                      str(bits_to_numbers(SOG) * 0.1) + ","
+                      str(bits_to_numbers(SOG) * 0.1) + "," + \
+                      str(bits_to_numbers(Position_Accuracy)) + ","
         if Longitude[0] == "0":
-            writeString += str(int(bits_to_numbers(Longitude[1:]) / 600000)) + "." + str(int((bits_to_numbers(Longitude[1:]) % 600000) / 60))
-            # print("Longitude: \t\t %d°.%d E"%(int(bits_to_numbers(Longitude[1:]) / 600000), int((bits_to_numbers(Longitude[1:]) % 600000) / 60)))
+            writeString += str(int(bits_to_numbers(Longitude[1:]) / 600000)) + "." + str(int((bits_to_numbers(Longitude[1:]) % 600000) / 60)) + ","
         else:
             return
-            # print("Longitude: \t\t %d°.%d W"%(int(bits_to_numbers(Longitude[1:]) / 600000), int((bits_to_numbers(Longitude[1:]) % 600000) / 60)))
-        writeString += ","
         if Latitude[0] == "0":
-            writeString += str(int(bits_to_numbers(Latitude[1:]) / 600000)) + "." + str(
-                int((bits_to_numbers(Latitude[1:]) % 600000) / 60))
-            # print("Latitude: \t\t %d°.%d N"%(int(bits_to_numbers(Latitude[1:]) / 600000), int((bits_to_numbers(Latitude[1:]) % 600000) / 60)))
+            writeString += str(int(bits_to_numbers(Latitude[1:]) / 600000)) + "." + str(int((bits_to_numbers(Latitude[1:]) % 600000) / 60)) + ","
         else:
             return
-            # print("Latitude: \t\t %d°.%d S"%(int(bits_to_numbers(Latitude[1:]) / 600000), int((bits_to_numbers(Latitude[1:]) % 600000) / 60)))
-        writeString += ","
-        writeString +=  str(bits_to_numbers(COG) * 0.1) + "," + \
-                        str(bits_to_numbers(HDG)) + "," + \
-                        str(bits_to_numbers(Time_Stamp)) + "," + \
-                        str(bits_to_numbers(Reserved_for_regional)) + "," + \
-                        str(RAIM_flag) + "," + \
-                        str(bits_to_numbers(Sync_state)) + "," + \
-                        str(bits_to_numbers(Slot_Timeout)) + "," + \
-                        str(bits_to_numbers(UTC_hour)) + "," + \
-                        str(bits_to_numbers(UTC_minute))
+        writeString += str(bits_to_numbers(COG) * 0.1) + "," + str(bits_to_numbers(HDG)) + "," + str(bits_to_numbers(Time_Stamp)) + "," + str(bits_to_numbers(Reserved_for_regional)) + "," + str(RAIM_flag) + ","
+        writeString += str(year) + "," + str(month) + "," + str(day) + "," + str(hour) + "," + str(minute) + "," + str(sec)
         writeString += "\r\n"
         f.write(writeString)
         log.write(message + "decode: " + writeString)
-        # print(message)
-        # print(writeString)
-        # print("Message Type: \t\t", bits_to_numbers(Message_Type))
-        # print("Repeat Indicator: \t", bits_to_numbers(Repeat_Indicator))
-        # print("MMSI: \t\t\t", bits_to_numbers(MMSI))
-        # print("Navigation Status: \t", bits_to_numbers(Navigation_Status))
-        # # TODO:ROT计算未完成，如下只输出其补码
-        # print("ROT: \t\t\t", bits_to_Complementnumbers(ROT))
-        # print("SOG: \t\t\t", bits_to_numbers(SOG) * 0.1, "kn")
-        # print("Position Accuracy: \t", Position_Accuracy)
-        # if Longitude[0] == "0":
-        #     print("Longitude: \t\t %d°.%d E"%(int(bits_to_numbers(Longitude[1:]) / 600000), int((bits_to_numbers(Longitude[1:]) % 600000) / 60)))
-        # else:
-        #     print("Longitude: \t\t %d°.%d W"%(int(bits_to_numbers(Longitude[1:]) / 600000), int((bits_to_numbers(Longitude[1:]) % 600000) / 60)))
-        # if Latitude[0] == "0":
-        #     print("Latitude: \t\t %d°.%d N"%(int(bits_to_numbers(Latitude[1:]) / 600000), int((bits_to_numbers(Latitude[1:]) % 600000) / 60)))
-        # else:
-        #     print("Latitude: \t\t %d°.%d S"%(int(bits_to_numbers(Latitude[1:]) / 600000), int((bits_to_numbers(Latitude[1:]) % 600000) / 60)))
-        # print("COG: \t\t\t", bits_to_numbers(COG) * 0.1)
-        # print("HDG: \t\t\t", bits_to_numbers(HDG))
-        # print("Time Stamp: \t\t", bits_to_numbers(Time_Stamp))
-        # print("Reserved for regional: \t", Reserved_for_regional)
-        # print("RAIM flag: \t\t", RAIM_flag)
-        # # print("Communication State", Communication_State)
-        # print("Sync state: \t\t", bits_to_numbers(Sync_state))
-        # print("Slot Timeout: \t\t", bits_to_numbers(Slot_Timeout))
-        # print("UTC hour: \t\t", bits_to_numbers(UTC_hour))
-        # print("UTC minute: \t\t", bits_to_numbers(UTC_minute))
+        id += 1
     else:
-        # TODO:27种编码剩余完成
-        # print("Message Type: \t\t", bits_to_numbers(Message_Type))
+        if bits_to_numbers(Message_Type) == 4 or bits_to_numbers(Message_Type) == 11:
+            if sum != 1 or cnt != 1:
+                return
+            if len(bits) < 168:
+                return
+            zhuanfa = bits[6:8] #2
+            MMSI = bits[8:38] #30
+            UTC_year = bits[38:52] #14
+            UTC_month = bits[52:56] #4
+            UTC_day = bits[56:61] #5
+            UTC_hour = bits[61:66] #5
+            UTC_min = bits[66:72] #6
+            UTC_second = bits[72:78] #6
+            Position_Accuracy = bits[78]  # 1
+            Longitude = bits[79:107]  # 28
+            Latitude = bits[107:134]  # 27
+            year = bits_to_numbers(UTC_year)
+            month = bits_to_numbers(UTC_month)
+            day = bits_to_numbers(UTC_day)
+            hour = bits_to_numbers(UTC_hour)
+            minute = bits_to_numbers(UTC_min)
+            sec = bits_to_numbers(UTC_second)
+        if bits_to_numbers(Message_Type) == 5:
+            if sum != 2 or cnt != 1:
+                return
+            tmpmessage = message
+            tmpbit = bits[:]
+            waitSingle = 5
         return
-
-# !AIVDM,1,1,0,B,16:b4BiP007WveP@taA000IN1P00,0*64
-# !AIVDM,1,1,,A,169FsD001o8ewMhF8Bb997A@05K8,0*26
-# !AIVDM,1,1,0,A,H6:VA;A0DpN1PT4pN3S3>222222,2*25
